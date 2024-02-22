12af485b02c15d7f9db72d28ba1c66ee75f5df09
Lorenzo Frinhani <115597607+EuLorenzo@users.noreply.github.com>
2024-02-22 19:45:19 -0300

    Arquivos
    Apenas colocando os arquivos iniciais no repositório

---
+++ script.js
@@ -0,0 +1,52 @@
+const chefs = [
+  {
+    nome: "Alain Ducasse",
+    idade: 65,
+    nacionalidade: "FR" // França
+  },
+  {
+    nome: "Gordon Ramsay",
+    idade: 55,
+    nacionalidade: "UK" // Reino Unido
+  },
+  {
+    nome: "Massimo Bottura",
+    idade: 59,
+    nacionalidade: "IT" // Itália
+  },
+  {
+    nome: "Thomas Keller",
+    idade: 66,
+    nacionalidade: "US" // Estados Unidos
+  },
+  {
+    nome: "Ferran Adrià",
+    idade: 59,
+    nacionalidade: "ES" // Espanha
+  },
+  {
+    nome: "Nobu Matsuhisa",
+    idade: 72,
+    nacionalidade: "JP" // Japão
+  },
+  {
+    nome: "Alex Atala",
+    idade: 53,
+    nacionalidade: "BR" // Brasil
+  },
+  {
+    nome: "Vikas Khanna",
+    idade: 49,
+    nacionalidade: "IN" // Índia
+  },
+  {
+    nome: "Yotam Ottolenghi",
+    idade: 53,
+    nacionalidade: "IL" // Israel
+  },
+  {
+    nome: "Curtis Stone",
+    idade: 46,
+    nacionalidade: "AU" // Austrália
+  }
+];
